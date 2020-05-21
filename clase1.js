a3357492947bdfb9e5d312e1dd11b7ec06f28658
jacob19891989 <jacob_orenday@hotmail.com>
2020-05-21 18:08:09 +0200

    Initial commit

---
+++ clase1.js
@@ -0,0 +1,74 @@
+function isGreaterThan(x) {
+    if (x > 3) {
+        return true;
+    } else if (x == 3) {
+        return false;
+    }  else {
+            return false;
+    }
+}
+
+console.log(isGreaterThan(3));
+
+3 == '4' false 
+
+codition1 || condition2
+
+condition1 && condition2 
+
+function isDividedBySix(num) {
+    if (num % 3 === 0 && num % 2== 0){
+        return true;
+    } else {
+        return false;
+    } 
+}
+
+console.log(isDividedBySix(53));
+
+//Métodos de strings
+
+let x = 'Mi perro se comió a james'
+x.length -> //regresa la longitud de 
+console.long();
+
+// toUpperCase
+let = 'sodifeweiosSDF sd';
+console.log(x.toUpperCase());
+
+
+//Ejercicio: Escribir una función que tenga entrada un string y regrese
+//string con los caracteres ordenados en orden alfabético. 
+
+function sortSorting(str); {
+    str.split('').sort().join('.')
+}
+
+//Array methods
+//sort()
+let x=['dasdfasdf', 'fasdfas', 'wfda'];
+console.log(x.join('_'));
+let x= '34';
+console.log(parseInt(x));
+
+//trim() -> quita el espacio a los lados de string
+
+//como poner una variable dentro de un string
+
+let x = 'cocacola';
+
+let y = `Jacob 
+
+function isUppercase(str) {
+
+}
+
+//ejercicio
+//is the string uppercase?
+//add the isUpperCase method to String to see wether the string is ALL CAPS.
+//For example 
+//returns true of false
+
+
+
+
